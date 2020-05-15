6f9126853660d1e5e7c00fdb65c906409c078546
AnnaSemibabnova <61233581+AnnaSemibabnova@users.noreply.github.com>
2020-05-15 22:30:37 +0300

    l

--- GeometricShapes/GeometricShapes/point.h
+++ GeometricShapes/GeometricShapes/point.h
@@ -1,1 +1,67 @@
+#include <iostream>
+#include"shapes.h"
 
+using namespace std;
+
+template <typename T>
+class Point :
+  public  Shapes
+{
+private:
+  T sign;
+public:
+  Point();
+  Point(T _sign);
+  Point(const Point& point);
+  
+  T Get();
+  float Perimeter() override;
+  
+  ostream& print(ostream& off) override;
+  bool operator ==(const Point& other);
+};
+
+template<typename T>
+inline Point<T>::Point()
+{
+  sign = '.';
+}
+
+template<typename T>
+inline Point<T>::Point(T _sign)
+{
+  sign = _sign;
+}
+
+template<typename T>
+inline Point<T>::Point(const Point& point)
+{
+  sign = point.sign;
+}
+
+template<typename T>
+inline T Point<T>::Get()
+{
+  return sign;
+}
+
+template<typename T>
+inline float Point<T>::Perimeter()
+{
+  return 0;
+}
+
+template<typename T>
+inline ostream& Point<T>::print(ostream& off)
+{
+  off << this->Get();
+  return off;
+}
+
+template<typename T>
+inline bool Point<T>::operator==(const Point& other)
+{
+  if (sign == other.sign)
+    return true;
+  return false;
+}
--- GeometricShapes/GeometricShapes/shapes.h
+++ GeometricShapes/GeometricShapes/shapes.h
@@ -1,1 +1,30 @@
+#include <iostream>
 
+using namespace std;
+
+class Shapes
+{
+public:
+  virtual ~Shapes() {}
+  virtual float Perimeter() = 0;
+  virtual ostream& print(ostream& off) = 0;
+  
+  friend ostream& operator<<(ostream& off, Shapes& another)
+  {
+    return another.print(off);
+  }
+  
+  bool operator > (Shapes& other)
+  {
+    if (this->Perimeter() > other.Perimeter())
+      return true;
+    return false;
+  }
+  
+  bool operator < (Shapes& other)
+  {
+    if (this->Perimeter() < other.Perimeter())
+      return true;
+    return false;
+  }
+};
